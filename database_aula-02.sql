d0d6945f0cd3b655340c2ad3b43c21cc69443bbf
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2023-08-18 17:04:15 -0300

    aaa

--- database_aula-02.sql
+++ database_aula-02.sql
@@ -9,7 +9,7 @@ create table produtos
     preco numeric
 );
 
-insert into produtos(id_produtos, nome, preco)vales
+insert into produtos(id_produtos, nome, preco)values
 (1, "iPhone 14", 5000),
 (2, "iPhone 13", 4500),
 (3, "iPhone 12", 4000),
@@ -24,8 +24,38 @@ insert into produtos(id_produtos, nome, preco)vales
 create table pedidos 
 (
 	id_pedidos int primary key,
-    id_produto int,
-    quantidade
+    id_produtos int,
+    quantidade int,
+    
+    -- criando referência para chave estrageira  
+    foreign key(id_produtos) references produtos (id_produtos)
 );
 
+insert into pedidos(id_pedidos, id_produtos, quantidade)values
+(1, 1, 280),
+(2, 2, 400),
+(3, 3, 80),
+(4, 4, 71),
+(5, 5, 15),
+(6, 6, 28),
+(7, 7, 33),
+(8, 8, 39),
+(9, 9, 47),
+(10, 10, 29);
+
 select * from produtos;
+
+select * from pedidos;
+
+-- criando relacionamento e conectando as tabelas
+select nome from produtos inner join pedidos on
+produtos.id_produtos=pedidos.id_produtos where id_pedidos=4;
+
+-- desafio // apelidando
+select p.id_produtos, quantidade, preco from produtos as p inner join pedidos as d on
+p.id_produtos=d.id_produtos where nome="iPhone X";
+
+
+-- KHAN ACADEMY
+
+ 
